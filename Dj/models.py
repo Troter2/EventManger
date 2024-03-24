2921662cfddfda385223dd2e69cf29f2645e0b66
Sergi Vila <47522460+Troter2@users.noreply.github.com>
2024-03-24 19:52:28 +0100

    audio dj added

--- Dj/models.py
+++ Dj/models.py
@@ -8,3 +8,4 @@ class Dj(models.Model):
     best_known = models.CharField(max_length=100)
     description = models.TextField()
     photo = models.ImageField(upload_to='dj/')
+    audio = models.FileField(upload_to='dj_audio/', null=True, blank=True)
