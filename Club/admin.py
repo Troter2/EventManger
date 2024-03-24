35cc82884bdacc1ce43f6f8dbd2b87c5cf6724f9
Josep Prades Vidal <jus25gamer@gmail.com>
2024-03-24 16:56:22 +0100

    club pages v1.0

--- Club/admin.py
+++ Club/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+
+from Club.models import Club
+
+admin.site.register(Club)
--- Club/views.py
+++ Club/views.py
@@ -7,15 +7,15 @@ from Club.models import Club
 # Create your views here.
 
 def club_list(request):
-    club = Club.objects.all()
-    return render(request, 'club/list.html', {'club': club})
+    clubs = Club.objects.all()
+    return render(request, 'Club/list.html', {'clubs': clubs})
 
 
-def club_detail(request):
+def club_detail(request, id):
     clubs = Club.objects.filter(id=id).first()
-    return render(request, 'local.html', {'clubs': clubs})
+    return render(request, 'Club/detail.html', {'clubs': clubs})
 
 
 @login_required
 def club_create(request):
-    return render(request, 'club/add.html')
+    return render(request, 'Club/add.html')
